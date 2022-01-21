467caeb61d222c9f2bf13a07d7f3a58783f41560
GitHub Action <action@github.com>
2022-01-21 22:31:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/zzehring/intellijjsonnet/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zzehring/intellijjsonnet/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zzehring.intellijjsonnet.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zzehring.intellijjsonnet.MyBundle
 
 class MyApplicationService {
 
