2d4f58ec3195f6ff36bd84a29b4e1ea8d0e654f8
Zack Zehring <zack.zehring@grafana.com>
2022-01-21 19:22:11 -0500

    Add initialization for jsonnet-language-server

--- src/main/kotlin/com/github/zzehring/intellijjsonnet/MyBundle.kt
+++ src/main/kotlin/com/github/zzehring/intellijjsonnet/MyBundle.kt
@@ -1,8 +1,12 @@
 package com.github.zzehring.intellijjsonnet
 
 import com.intellij.DynamicBundle
+import com.intellij.openapi.application.PreloadingActivity
+import com.intellij.openapi.progress.ProgressIndicator
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
+import org.wso2.lsp4intellij.IntellijLanguageClient
+import org.wso2.lsp4intellij.client.languageserver.serverdefinition.RawCommandServerDefinition
 
 @NonNls
 private const val BUNDLE = "messages.MyBundle"
@@ -18,4 +22,13 @@ object MyBundle : DynamicBundle(BUNDLE) {
     @JvmStatic
     fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
         getLazyMessage(key, *params)
+}
+
+object JsonnetLSPreloadingActivity : PreloadingActivity() {
+    override fun preload(indicator: ProgressIndicator) {
+        IntellijLanguageClient.addServerDefinition(RawCommandServerDefinition(
+            "jsonnet,libsonnet",
+            arrayOf("/Users/zzehring/work/projects/grafana_org/hackathon/jsonnet-language-server/jsonnet-language-server")))
+    }
+
 }
