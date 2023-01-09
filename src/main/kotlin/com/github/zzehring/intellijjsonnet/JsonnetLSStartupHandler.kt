5c6d6118d4ca8c9f7f2ef7a692b0e42869906459
Zack Zehring <zack.zehring@grafana.com>
2023-01-09 14:16:57 -0500

    Migrate PreloadingActivity to BackgroundStartupActivity + LS handler
    PreloadingActivity has been deprecated and removed ing 223.* IDE releases.
    To keep feature parity, we now use BackgroundStartupActivity instead.
    Also, break out the Jsonnet Language Server startup logic into its own class
    and have a lightweight BackGroundStartupActivity class to just call the handler.
    
    Since we upgraded to 2.2.1 for ktor, some refactoring was needed to use
    the updated APIs, but the behavior should remain the same.

--- src/main/kotlin/com/github/zzehring/intellijjsonnet/JsonnetLSStartupHandler.kt
+++ src/main/kotlin/com/github/zzehring/intellijjsonnet/JsonnetLSStartupHandler.kt
@@ -3,24 +3,24 @@ package com.github.zzehring.intellijjsonnet
 import com.github.zzehring.intellijjsonnet.releases.Asset
 import com.github.zzehring.intellijjsonnet.releases.RepoRelease
 import com.github.zzehring.intellijjsonnet.settings.JLSSettingsStateComponent
-import com.intellij.notification.*
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationAction
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.application.PathManager
-import com.intellij.openapi.application.PreloadingActivity
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.util.system.CpuArch
 import com.intellij.util.text.SemVer
-import io.ktor.client.*
+import io.ktor.client.HttpClient
 import io.ktor.client.call.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.features.json.*
-import io.ktor.client.features.json.serializer.*
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
 import org.wso2.lsp4intellij.IntellijLanguageClient
-import org.wso2.lsp4intellij.IntellijLanguageClient.getProjectToLanguageWrappers
 import org.wso2.lsp4intellij.client.languageserver.serverdefinition.RawCommandServerDefinition
 import org.wso2.lsp4intellij.listeners.LSPProjectManagerListener
 import org.wso2.lsp4intellij.utils.FileUtils
@@ -37,25 +37,28 @@ const val EXTENSIONS = "jsonnet,libsonnet"
 
 data class TargetReleaseInfo(val tag: String, val downloadUrl: String)
 
-object JsonnetLSPreloadingActivity : PreloadingActivity() {
-    private val LOG = Logger.getInstance(
+class JsonnetLSStartupHandler {
+
+    private val log = Logger.getInstance(
         LSPProjectManagerListener::class.java
     )
-    override fun preload(indicator: ProgressIndicator) {
+
+    fun start() {
+
         val languageServerRepo = JLSSettingsStateComponent.instance.state.releaseRepository
         val platform = getPlatform()
         val arch = getArch()
 
-        LOG.info("Running on -> platform: $platform ; arch: $arch")
+        log.info("Running on -> platform: $platform ; arch: $arch")
 
         val releaseURL = URL("https://api.github.com/repos/${languageServerRepo}/releases/latest")
 
-        LOG.info("Repo/release URL: $releaseURL")
+        log.info("Repo/release URL: $releaseURL")
 
         val httpClient = HttpClient(CIO) {
             expectSuccess = false
-            install(JsonFeature) {
-                serializer = KotlinxSerializer(kotlinx.serialization.json.Json {
+            install(ContentNegotiation) {
+                json(Json {
                     prettyPrint = true
                     isLenient = true
                 })
@@ -63,7 +66,7 @@ object JsonnetLSPreloadingActivity : PreloadingActivity() {
         }
 
         val repoInfo = getLatestReleaseInfo(httpClient, releaseURL, platform, arch)
-        LOG.info("Latest tag: ${repoInfo.tag} ; Download URL: ${repoInfo.downloadUrl}")
+        log.info("Latest tag: ${repoInfo.tag} ; Download URL: ${repoInfo.downloadUrl}")
 
         val binFile = File(PathManager.getPluginsPath().plus("/Jsonnet Language Server/jsonnet-language-server"))
 
@@ -83,7 +86,8 @@ object JsonnetLSPreloadingActivity : PreloadingActivity() {
         IntellijLanguageClient.addServerDefinition(
             RawCommandServerDefinition(
                 EXTENSIONS,
-                arrayOf(binFile.toString(), "--tanka", "--lint"))
+                arrayOf(binFile.toString(), "--tanka", "--lint")
+            )
         )
     }
 
@@ -97,7 +101,7 @@ object JsonnetLSPreloadingActivity : PreloadingActivity() {
         val versionOutput = p.inputStream.bufferedReader().readText().trim()
         val currentVersion = SemVer.parseFromText(versionOutput.split(" ").last())
         val latestVersion = SemVer.parseFromText(tag.trimStart('v'))
-        LOG.info("Getting comparison between $currentVersion and $tag")
+        log.info("Getting comparison between $currentVersion and $tag")
         return if (currentVersion != null && latestVersion != null) {
             !currentVersion.isGreaterOrEqualThan(latestVersion)
         } else {
@@ -107,9 +111,13 @@ object JsonnetLSPreloadingActivity : PreloadingActivity() {
 
     private fun presentUpdateBalloon(httpClient: HttpClient, binFile: File, repoInfo: TargetReleaseInfo) {
         val project = ProjectManager.getInstance().defaultProject
-        Notification("lsp", "New jsonnet-language-server version (${repoInfo.tag}) available. Would you like to update?", NotificationType.IDE_UPDATE)
+        Notification(
+            "lsp",
+            "New jsonnet-language-server version (${repoInfo.tag}) available. Would you like to update?",
+            NotificationType.IDE_UPDATE
+        )
             .addAction(NotificationAction.createSimpleExpiring("Update") { download(httpClient, binFile, repoInfo) })
-            .addAction(NotificationAction.createSimpleExpiring("Cancel") { LOG.info("User declined update.") })
+            .addAction(NotificationAction.createSimpleExpiring("Cancel") { log.info("User declined update.") })
             .notify(project)
     }
 
@@ -119,12 +127,15 @@ object JsonnetLSPreloadingActivity : PreloadingActivity() {
             os.contains("win") -> {
                 "windows"
             }
+
             os.contains("nix") || os.contains("nux") || os.contains("aix") -> {
                 "linux"
             }
+
             os.contains("mac") -> {
                 "darwin"
             }
+
             else -> "linux"
         }
     }
@@ -135,18 +146,25 @@ object JsonnetLSPreloadingActivity : PreloadingActivity() {
             arch.equals(CpuArch.ARM64) -> {
                 "arm64"
             }
+
             arch.equals(CpuArch.X86_64) -> {
                 "amd64"
             }
+
             else -> "amd64"
         }
     }
 
-    private fun getLatestReleaseInfo(httpClient: HttpClient, releaseURL: URL, platform: String, arch: String): TargetReleaseInfo {
+    private fun getLatestReleaseInfo(
+        httpClient: HttpClient,
+        releaseURL: URL,
+        platform: String,
+        arch: String
+    ): TargetReleaseInfo {
         var binaryDownloadUrl = ""
         var latestTag: String
         runBlocking {
-            val release: RepoRelease = httpClient.get(releaseURL)
+            val release: RepoRelease = httpClient.get(releaseURL).body()
             latestTag = release.tag_name
             for (asset: Asset in release.assets) {
                 if (arch in asset.name && platform in asset.name) {
@@ -162,22 +180,26 @@ object JsonnetLSPreloadingActivity : PreloadingActivity() {
         runBlocking {
             stopServers()
             val httpResponse: HttpResponse = httpClient.get(repoInfo.downloadUrl)
-            val responseBody: ByteArray = httpResponse.receive()
+            val responseBody: ByteArray = httpResponse.body()
             binFile.writeBytes(responseBody)
-            LOG.info("Saved binary to ${binFile.name}")
+            log.info("Saved binary to ${binFile.name}")
             val project = ProjectManager.getInstance().defaultProject
-            Notification("lsp", "Language Server jsonnet-language-server (version ${repoInfo.tag}) downloaded", NotificationType.IDE_UPDATE)
+            Notification(
+                "lsp",
+                "Language Server jsonnet-language-server (version ${repoInfo.tag}) downloaded",
+                NotificationType.IDE_UPDATE
+            )
                 .notify(project)
             FileUtils.reloadAllEditors()
         }
     }
 
     private fun stopServers() {
-        getProjectToLanguageWrappers().forEach { (_, servers) ->
+        IntellijLanguageClient.getProjectToLanguageWrappers().forEach { (_, servers) ->
             servers.forEach { server ->
-                LOG.info("Stopping server with status:'${server.status}' and project: ${server.project})}")
+                log.info("Stopping server with status:'${server.status}' and project: ${server.project})}")
                 server.stop(true)
-                LOG.info("Stopped server. New status:'${server.status}'")
+                log.info("Stopped server. New status:'${server.status}'")
             }
         }
     }
