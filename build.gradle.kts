81ef5bc7bd5063adc74f823d82c02c06f4d7e1ea
Zack Zehring <zack.zehring@grafana.com>
2023-08-02 14:42:51 -0400

    build: remove qodana throughout repo
    Qodana code inspection is long and not very helpful. Remove any traces
    of it from the repo.
    
    It was also causing problems when running builds, so win-win.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,6 @@ plugins {
     id("org.jetbrains.intellij") version "1.11.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.0.0"
-    // Gradle Qodana Plugin
-    id("org.jetbrains.qodana") version "0.1.13"
     // Kotlin Serializer Plugin
     id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
 }
@@ -55,14 +53,6 @@ intellij {
 changelog {
     version.set(properties("pluginVersion"))
     groups.set(emptyList())
-}
-
-// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
-qodana {
-    cachePath.set(projectDir.resolve(".qodana").canonicalPath)
-    reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
-    saveReport.set(true)
-    showReport.set(System.getenv("QODANA_SHOW_REPORT")?.toBoolean() ?: false)
 }
 
 tasks {
