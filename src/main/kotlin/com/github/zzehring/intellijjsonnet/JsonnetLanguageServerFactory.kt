f0eade9250a81ed2a641080c6e9a4a1a0569a150
Felix Berlakovich <felix@berlakovich.at>
2026-01-20 11:09:53 +0100

    Use lsp4j update facility

--- src/main/kotlin/com/github/zzehring/intellijjsonnet/JsonnetLanguageServerFactory.kt
+++ src/main/kotlin/com/github/zzehring/intellijjsonnet/JsonnetLanguageServerFactory.kt
@@ -2,12 +2,24 @@ package com.github.zzehring.intellijjsonnet
 
 import com.intellij.openapi.project.Project
 import com.redhat.devtools.lsp4ij.LanguageServerFactory
+import com.redhat.devtools.lsp4ij.installation.ServerInstaller
 import com.redhat.devtools.lsp4ij.server.StreamConnectionProvider
 import org.jetbrains.annotations.NotNull
+import org.jetbrains.annotations.Nullable
 
 class JsonnetLanguageServerFactory : LanguageServerFactory {
 
     override fun createConnectionProvider(@NotNull project: Project): StreamConnectionProvider {
         return JsonnetLanguageServer(project)
+    }
+
+    /**
+     * Creates a server installer for global scope installation.
+     * The jsonnet-language-server binary is installed to ~/.lsp4ij/lsp/jsonnet-language-server/
+     * and shared across all projects.
+     */
+    @Nullable
+    override fun createServerInstaller(): ServerInstaller {
+        return JsonnetServerInstaller()
     }
 }
